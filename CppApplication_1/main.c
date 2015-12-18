fb7003b3e921537579886606199bb9eb3233e564
ETFBL <nickyleeyy@linuxmail.org>
2015-12-18 12:46:29 +0100

    test

---
+++ CppApplication_1/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Student
+ *
+ * Created on December 18, 2015, 12:45 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Hello");
+    return (EXIT_SUCCESS);
+}
+
